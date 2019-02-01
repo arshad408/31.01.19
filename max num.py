e3cb21fb731a54913245531daded0f3ded60c153
arshad408 <arshadachu408@gmail.com>
2019-02-01 12:33:02 +0530

    Update max num.py

--- max num.py
+++ max num.py
@@ -1,7 +1,7 @@
-a=[]
-n=int(input("Enter number of elements:"))
+y=[]
+n=int(input("Enter num of elements:"))
 for i in range(1,n+1):
-    b=int(input("Enter element:"))
-    a.append(b)
-a.sort()
-print("Largest element is:",a[n-1])
+    x=int(input("Enter the element:"))
+    x.append(b)
+y.sort()
+print("Largest element is:",y[n-1])
